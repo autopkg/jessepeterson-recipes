24c3838fca4ed031bea749036dc3d9a20080495b
Jesse Peterson <jesse@mactechs.com>
2013-10-15 16:04:00 -0700

    Change to checking CUDA update URL: related to #2

--- NVIDIA/CUDADriverURLProvider.py
+++ NVIDIA/CUDADriverURLProvider.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 
-import re
+import os
 import urllib2
 
 from autopkglib import Processor, ProcessorError
 
+from Foundation import NSPredicate
+import FoundationPlist
+
 __all__ = ["CUDADriverURLProvider"]
 
-CHECK_URL = 'http://www.nvidia.com/object/cuda-mac-driver.html'
-BASE_DL_URL = 'http://us.download.nvidia.com'
-
-re_dl = re.compile('confirmation\.php\?url\=(.*)\&lang')
+CHECK_URL = 'https://partners.download.nvidia.com/activation/cuda_update_macos.xml'
+PLIST_FN  = 'cuda_update_macos.plist'
 
 class CUDADriverURLProvider(Processor):
 	'''Provides URL to the latest CUDA Driver download from NVIDIA.'''
 
 	input_variables = {
-		'base_url': {
+		'cuda_os_ver': {
 			'required': False,
-			'description': 'Base Download URL for DMGs',
+			'description': 'Version of OS to test the CUDA update NSPredicate with. Defaults to currently shipping Apple Mac OS X version.',
 			},
 	}
 	output_variables = {
@@ -29,29 +30,50 @@ class CUDADriverURLProvider(Processor):
 
 	description = __doc__
 
-	def get_url(self, base_url):
+	def get_url(self, os_ver):
 		try:
 			f = urllib2.urlopen(CHECK_URL)
-			html = f.read()
+			plist_text = f.read()
 			f.close()
 		except BaseException as e:
 			raise ProcessorError('Could not retrieve check URL %s' % CHECK_URL)
 
-		m = re_dl.search(html)
+		plist_filename = os.path.join(self.env['RECIPE_CACHE_DIR'], PLIST_FN)
 
-		if not m:
-			raise ProcessorError('Could not find (match) latest version in check URL!')
+		try:
+			plistf = open(plist_filename, 'w')
+			plistf.write(plist_text)
+			plistf.close()
+		except:
+			raise ProcessorError('Could not write NVIDIA plist file %s' % plist_filename)
 
-		return base_url + m.group(1)
+		try:
+			plist = FoundationPlist.readPlist(plist_filename)
+		except:
+			raise ProcessorError('Could not read NVIDIA plist file %s' % plist_filename)
+
+		# the Version is blank here due to the plist NSPredicate
+		# testing it to not be the current version.
+		pred_obj = {'Ticket': {'Version': ''}, 'SystemVersion': {'ProductVersion': os_ver}}
+
+		for rule in plist['Rules']:
+			try:
+				predicate = NSPredicate.predicateWithFormat_(rule['Predicate'])
+			except:
+				raise ProcessorError('Problem with NSPredicate: %s' % rule['Predicate'])
+	
+			if predicate.evaluateWithObject_(pred_obj):
+				return rule['Codebase']
+			
+		raise ProcessorError('No valid Predicate rules found!')
 
 	def main(self):
-		# Determine base_url.
-		if "base_url" in self.env:
-			base_url = self.env.base_url
+		if 'cuda_os_ver' in self.env:
+			cuda_os_ver = self.env['cuda_os_ver']
 		else:
-			base_url = BASE_DL_URL
+			cuda_os_ver = '10.8'
 
-		self.env['url'] = self.get_url(base_url)
+		self.env['url'] = self.get_url(cuda_os_ver)
 		self.output('File URL %s' % self.env['url'])
 
 if __name__ == '__main__':
