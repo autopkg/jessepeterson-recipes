2c31eaacb32f0d538601b490487d08f2bc7f4e69
Jesse Peterson <jesse@mactechs.com>
2014-02-16 20:08:23 -0800

    Added predicate installer

---
+++ PredicateInstaller/TimestampVersioner.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+#!/usr/bin/env python
+
+from time import time
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = ["TimestampVersioner"]
+
+class TimestampVersioner(Processor):
+	'''Returns a version of the current'''
+
+	input_variables = {
+		'version_major': {
+			'description': 'Major component of version',
+			'required': False,
+		}
+	}
+	output_variables = {
+		'version': {
+			'description': 'Pseudo-version of the format <maj>.<min>. Maj is default 0 and min is the current epoch time.'
+		},
+	}
+
+	description = __doc__
+
+	def main(self):
+		vmaj = self.env.get('version_major', '0')
+		vmin = int(time())
+
+		version = '%s.%d' % (vmaj, vmin)
+
+		self.output('Version is %s' % version)
+
+		self.env['version'] = version
+
+if __name__ == '__main__':
+	processor = TimestampVersioner()
+	processor.execute_shell()
