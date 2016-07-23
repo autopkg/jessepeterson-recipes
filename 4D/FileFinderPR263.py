e4fb3c3637503c24cf060394f23e09f4bffb01f3
Jesse Peterson <jesse@mactechs.com>
2016-07-23 13:25:53 -0700

    4D recipe updates: add CodeSignatureVerifier, apply patched FileFinder from autopkg/autopkg#263, integrate Versioner processor

---
+++ 4D/FileFinderPR263.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+#
+# Copyright 2013 Jesse Peterson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""See docstring for FileFinderPR263 class"""
+
+
+from glob import glob
+from autopkglib import Processor, ProcessorError
+from autopkglib.DmgMounter import DmgMounter
+import os.path
+
+__all__ = ["FileFinderPR263"]
+
+class FileFinderPR263(DmgMounter):
+    '''Finds a filename for use in other Processors.
+
+Currently only supports glob filename patterns.
+'''
+
+    input_variables = {
+        'pattern': {
+            'description': 'Shell glob pattern to match files by',
+            'required': True,
+        },
+        'find_method': {
+            'description': ('Type of pattern to match. Currently only '
+                            'supported type is "glob" (also the default)'),
+            'default': 'glob',
+            'required': False,
+        },
+    }
+    output_variables = {
+        'found_filename': {
+            'description': 'Full path of found filename',
+        },
+        'dmg_found_filename': {
+            'description': 'DMG-relative path of found filename',
+        }
+    }
+
+    description = __doc__
+
+    def globfind(self, pattern):
+        '''If multiple files are found the last alphanumerically sorted found
+        file is returned'''
+        #pylint: disable=no-self-use
+
+        glob_matches = glob(pattern)
+
+        if len(glob_matches) < 1:
+            raise ProcessorError('No matching filename found')
+
+        glob_matches.sort()
+
+        return glob_matches[-1]
+
+    def main(self):
+        pattern = self.env.get('pattern')
+
+        method = self.env.get('find_method')
+
+        if method != 'glob':
+            raise ProcessorError('Unsupported find_method: %s' % method)
+
+        source_path = pattern
+
+        # Check if we're trying to copy something inside a dmg.
+        (dmg_path, dmg, dmg_source_path) = self.parsePathForDMG(source_path)
+        try:
+            if dmg:
+                # Mount dmg and copy path inside.
+                mount_point = self.mount(dmg_path)
+                source_path = os.path.join(mount_point, dmg_source_path)
+            # process path with globbing
+            match = self.globfind(source_path)
+            self.env['found_filename'] = match
+            self.output("Found file match: '%s' from globbed '%s'" % (self.env['found_filename'], source_path))
+
+            if dmg and match.startswith(mount_point):
+                self.env['dmg_found_filename'] = match[len(mount_point):].lstrip(os.path.sep)
+                self.output("DMG-relative file match: '%s'" % self.env['dmg_found_filename'])
+        finally:
+            if dmg:
+                self.unmount(dmg_path)
+
+
+if __name__ == '__main__':
+    PROCESSOR = FileFinderPR263()
+    PROCESSOR.execute_shell()
